2c8df4c601c1bfdb8590ab37a5f99bdc8afc8b00
lichenglonaa$ <34432269+lichenglon@users.noreply.github.com>
2018-01-02 16:41:38 +0800

    house detail lister

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,25 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//首页
 Route::get('/','IndexController@index');
 
+//用户
 Route::group(['prefix'=>'user'],function() {
 	$controller = 'User\UserController@';
 	#登陆页
 	Route::get('login',$controller.'login');
 	#注册页
 	Route::get('register',$controller.'register');
-	#注册保存
-	Route::post('save',$controller.'save');
 
-	//登录检测
-	Route::post('display',$controller.'display');
+});
+
+//房源
+Route::group(['prefix'=>'house'],function() {
+	$controller = 'House\HouseController@';
+	#列表页
+	Route::get('listing',$controller.'listing');
+	#详情页
+	Route::get('detail/{id?}',$controller.'detail');
 
 });
